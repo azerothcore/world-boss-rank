9c57c5c921652d6ac33aaea2ee510a8aaa2a8a58
Helias <stefanoborzi32@gmail.com>
2021-07-26 21:38:35 +0200

    chore: add 2 bosses

--- src/app/utils/bosses.ts
+++ src/app/utils/bosses.ts
@@ -5,4 +5,6 @@ export const BOSSES = [
   { id: 1112003, name: 'Zombie Captain', },
   { id: 1112031, name: 'Crocodile Bunbee', },
   { id: 1112033, name: 'Crokolisk Minion', },
+  { id: 1112021, name: 'Crocolisk Dundee', },
+  { id: 1112023, name: 'Aligator Minion', },
 ];
